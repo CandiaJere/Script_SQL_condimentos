3ad35362c5262060ca14a968e56122d930a44d9d
CandiaJere <108201160+CandiaJere@users.noreply.github.com>
2023-10-30 11:17:55 -0300

    Update stored_procedure.sql

--- stored_procedure.sql
+++ stored_procedure.sql
@@ -4,8 +4,9 @@ CREATE PROCEDURE
   calculo_precio_total (IN precio_unitario DECIMAL(10,2),
                         IN cantidad        INT,
                         IN descuento       DECIMAL(10,2))
-begin
-  DECLARE precio_total DECIMAL(10,2);SET precio_total = precio_unitario * cantidad * (1 - descuento);SELECT precio_total AS 'precio_total';END $$
+BEGIN
+  DECLARE precio_total DECIMAL(10,2);SET precio_total = precio_unitario * cantidad * (1 - descuento);SELECT precio_total AS 'precio_total';
+END $$
 DELIMITER;
 -- CALL calculo_precio_total (10.00,5,0.1);
 
@@ -22,7 +23,7 @@ CREATE PROCEDURE
                   IN provincia      VARCHAR(50),
                   IN email          VARCHAR(100)
                   )
-begin
+BEGIN
   INSERT INTO clientes
               (
                           cliente_id,
@@ -42,7 +43,8 @@ begin
                           ciudad,
                           provincia,
                           email
-              );END $$
+              );
+END $$
 DELIMITER;
 -- CALL InsertarCliente(100, 'Nombre del Cliente', '123456789', 'Av. Roca 1233', 'Ciudad', 'Provincia', 'cliente@mail.com');
 
@@ -51,9 +53,10 @@ DELIMITER;
 DELIMITER $$
 CREATE PROCEDURE
   eliminarcliente (IN cliente_id INT)
-begin
+BEGIN
   DELETE
   FROM   clientes
-  WHERE  cliente_id = cliente_id;END $$
+  WHERE  cliente_id = cliente_id;
+END $$
 DELIMITER ;
 -- CALL EliminarCliente(1);
