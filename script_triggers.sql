2adbc165b83c40f2eadbf2adad08e87cbae8a783
CandiaJere <108201160+CandiaJere@users.noreply.github.com>
2023-11-03 17:32:10 -0300

    Update script_triggers.sql

--- script_triggers.sql
+++ script_triggers.sql
@@ -27,8 +27,6 @@ DELIMITER ;
 
 
 
-
-
 -- Crear la tabla de registro
 CREATE TABLE Registro_Productos (
     Accion VARCHAR(10) NOT NULL,
@@ -51,3 +49,37 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+
+
+--- Trigger si la tabla pedido tiene un Cliente_ID null
+
+
+CREATE TABLE Log_Pedido (
+    Log_ID INT AUTO_INCREMENT,
+    Pedido_ID INT NOT NULL,
+    Cliente_ID INT NOT NULL,
+    Empleado_ID INT NOT NULL,
+    Fecha DATE NOT NULL,
+    Fecha_Modificacion TIMESTAMP,
+    PRIMARY KEY (Log_ID)
+);
+
+
+DELIMITER //
+CREATE TRIGGER Pedido_Before_Insert
+BEFORE INSERT ON Pedido
+FOR EACH ROW
+BEGIN
+    IF NEW.Cliente_ID IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'No se puede insertar un pedido sin especificar un Cliente_ID';
+    ELSE
+        -- Insertar en la tabla de registro antes de la inserción
+        INSERT INTO Log_Pedido (Pedido_ID, Cliente_ID, Empleado_ID, Fecha, Fecha_Modificacion)
+        VALUES (NEW.Pedido_ID, NEW.Cliente_ID, NEW.Empleado_ID, NEW.Fecha, NOW());
+    END IF;
+END;
+//
+DELIMITER ;
+
