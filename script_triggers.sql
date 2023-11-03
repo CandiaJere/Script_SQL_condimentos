55a71b3130965990be590c1ba0781fa1ab4ddc9c
CandiaJere <108201160+CandiaJere@users.noreply.github.com>
2023-11-03 15:31:06 -0300

    Update script_triggers.sql

--- script_triggers.sql
+++ script_triggers.sql
@@ -23,3 +23,31 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+
+
+
+
+
+-- Crear la tabla de registro
+CREATE TABLE Registro_Productos (
+    Accion VARCHAR(10) NOT NULL,
+    Producto_ID INT NOT NULL,
+    Nombre_Producto VARCHAR(50),
+    Proveedor_ID INT,
+    Categoria_ID INT,
+    Precio FLOAT,
+    Fecha TIMESTAMP
+);
+
+-- Crear un trigger after para insertar registros en la tabla de registro
+DELIMITER //
+CREATE TRIGGER LOG_Productos
+AFTER INSERT ON Productos
+FOR EACH ROW
+BEGIN
+    INSERT INTO Registro_Productos (Accion, Producto_ID, Nombre_Producto, Proveedor_ID, Categoria_ID, Precio, Fecha)
+    VALUES ('INSERT', NEW.Producto_ID, NEW.Nombre_Producto, NEW.Proveedor_ID, NEW.Categoria_ID, NEW.Precio, NOW());
+END;
+//
+DELIMITER ;
